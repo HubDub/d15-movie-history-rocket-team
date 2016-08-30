672ef66a283494c52a269410add5de31549ec2dd
Jamie Duke <jamsduke@gmail.com>
2016-08-30 14:48:08 -0500

    indent fix

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ $(document).on("click", ".switchViewButton", function() {
   //this toggles the slider to only show when the watched movies button is clicked
   console.log(this.id);
   if (this.id === "watchedMoviesButton") {
-    $("#showSlider").toggleClass("hidden")
+    $("#showSlider").toggleClass("hidden");
   } else $("#showSlider").addClass("hidden");
 
   $(this).siblings('.btn').removeClass('btn-primary');
